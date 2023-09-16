0929745c81ddc6d86ed83c0309ce91637f4f004a
hms <mshan2923@gmail.com>
2023-09-16 15:30:21 +0900

    Inti

---
+++ src/main/java/com/example/modutest/ModuTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.modutest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuTestApplication.class, args);
+    }
+
+}
