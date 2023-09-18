e39f64b1371bcf6b608afded0932a4b0ee2be4f2
Jinwoong <zizrar2@gmail.com>
2023-09-18 15:51:48 +0900

    feat: Entity 추가

--- src/main/java/com/example/modutest/entity/TestElement/Tester.java
+++ src/main/java/com/example/modutest/entity/TestElement/Tester.java
@@ -1,25 +1,50 @@
 package com.example.modutest.entity.TestElement;
 
+import com.example.modutest.entity.Comment;
+import com.example.modutest.entity.User;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-@Table()
 @Getter
+@Setter
+@Table(name = "tester")
+@NoArgsConstructor
 public class Tester {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false)
-    private String title;
-    @Column(nullable = false)
-    private String content;
-    @Column(nullable = true)
-    private String image;
-    @Column(nullable = false)
-    private Long views;
-    @Column(nullable = false)
-    private Long like;
 
-    //생성한 유저 정보 FK
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "image", nullable = true)
+    private String image;
+
+    @Column(name = "views", nullable = false)
+    private Long views;
+
+    @Column(name = "likes", nullable = false)
+    private Long likes;
+
+    // User FK - 하나의 유저가 여러 테스트를 생성
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User userId;
+
+    // Test FK - 하나의 테스트가 여러 질문 생성
+    @OneToMany(mappedBy = "tester", cascade = CascadeType.ALL)
+    private List<Question> questions = new ArrayList<>();
+
+    // Comment FK - 하나의 테스트에 여러 댓글
+    @OneToMany(mappedBy = "tester", cascade = CascadeType.ALL)
+    private List<Comment> comments = new ArrayList<>();
 }
--- src/main/java/com/example/modutest/repository/ComentRepository.java
+++ src/main/java/com/example/modutest/repository/ComentRepository.java
@@ -1,7 +1,7 @@
 package com.example.modutest.repository;
 
-import com.example.modutest.entity.Coment;
+import com.example.modutest.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ComentRepository extends JpaRepository<Coment, Long> {
+public interface ComentRepository extends JpaRepository<Comment, Long> {
 }
