05508600731ffd8a59ef118f39e75ffddbea9ce2
mshan2923 <mshan2923@gmail.com>
2023-09-21 00:11:35 +0900

    Fix : Reimport

--- src/main/java/com/example/modutest/entity/TestElement/Choice.java
+++ src/main/java/com/example/modutest/entity/TestElement/Choice.java
@@ -1,5 +1,6 @@
 package com.example.modutest.entity.TestElement;
 
+import com.example.modutest.dto.TestElement.ChoiceDto;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,11 +22,16 @@ public class Choice {
     @Column(name = "isCorrect",nullable = false)
     private boolean isCorrect;
 
-    @Column(name = "score",nullable = false)
-    private int score;
+//    @Column(name = "score",nullable = false)
+//    private int score;
 
     //질문 FK -> 질문 하나에 여러 선택지
     @ManyToOne
     @JoinColumn(name = "questionId")
     private Question question;
+
+    public Choice(ChoiceDto dto){
+        this.content = dto.getContent();
+        this.isCorrect = dto.isCorrect();
+    }
 }
