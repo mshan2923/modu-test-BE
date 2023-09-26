d8b5a5d7e5276d6f0c61a2808159820c661bbf4e
mshan2923 <mshan2923@gmail.com>
2023-09-26 12:42:26 +0900

    Fetch : Reimport fix

--- src/main/java/com/example/modutest/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.modutest.repository;
-
-import com.example.modutest.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
--- src/main/java/com/example/modutest/repository/TesterRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.modutest.repository;
-
-import com.example.modutest.entity.TestElement.Tester;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TesterRepository extends JpaRepository<Tester, Long> {
-}
